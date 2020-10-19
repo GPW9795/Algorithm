ee89970e5e4186d1aa82be17ce8334d450a456e3
gao peiwen <vivian9795@163.com>
2020-10-19 15:03:39 +0800

    基本概念

--- 03-图/src/com/mj/graph/ListGraph.java
+++ 03-图/src/com/mj/graph/ListGraph.java
@@ -1,9 +1,9 @@
 package com.mj.graph;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
-public class ListGraph<V,E> implements Graph<V,E> {
+public class ListGraph<V, E> implements Graph<V, E> {
+    private Map<V, Vertex<V, E>> vertices = new HashMap<>();
 
     @Override
     public int edgeSize() {
@@ -12,12 +12,13 @@ public class ListGraph<V,E> implements Graph<V,E> {
 
     @Override
     public int verticesSize() {
-        return 0;
+        return vertices.size();
     }
 
     @Override
     public void addVertex(V v) {
-
+        if (vertices.containsKey(v)) return;
+        vertices.put(v, new Vertex<>(v));
     }
 
     @Override
@@ -27,11 +28,26 @@ public class ListGraph<V,E> implements Graph<V,E> {
 
     @Override
     public void addEdge(V from, V to) {
-
+        addEdge(from, to, null);
     }
 
     @Override
     public void addEdge(V from, V to, E weight) {
+        // 判断顶点是否存在
+        Vertex<V, E> fromVertex = vertices.get(from);
+        if (fromVertex == null) {
+            fromVertex = new Vertex<>(from);
+            vertices.put(from, fromVertex);
+        }
+        Vertex<V, E> toVertex = vertices.get(to);
+        if (toVertex == null) {
+            toVertex = new Vertex<>(to);
+            vertices.put(to, toVertex);
+        }
+        // 判断边是否存在
+        if (fromVertex.outEdges.contains(toVertex)) {
+
+        }
 
     }
 
@@ -42,13 +58,39 @@ public class ListGraph<V,E> implements Graph<V,E> {
 
     private static class Vertex<V, E> {
         V value;
-        Set<Edge<V,E>> inEdges = new HashSet<>();
-        Set<Edge<V,E>> outEdges = new HashSet<>();
+        Set<Edge<V, E>> inEdges = new HashSet<>();
+        Set<Edge<V, E>> outEdges = new HashSet<>();
+
+        public Vertex(V value) {
+            this.value = value;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            return Objects.equals(value, ((Vertex<V, E>) o).value);
+        }
+
+        @Override
+        public int hashCode() {
+            return value == null ? 0 : value.hashCode();
+        }
     }
 
     private static class Edge<V, E> {
         Vertex<V, E> from;
         Vertex<V, E> to;
         E weight;
+
+        @Override
+        public boolean equals(Object o) {
+            Edge<V, E> edge = (Edge<V, E>) o;
+            return Objects.equals(from, edge.from) &&
+                    Objects.equals(to, edge.to);
+        }
+
+        @Override
+        public int hashCode() {
+            return from.hashCode() * 31 + to.hashCode();
+        }
     }
 }
