ae6fcaa209dc8f481b59a00d5210a5f7238ee7d2
gao peiwen <vivian9795@163.com>
2020-10-19 17:24:30 +0800

    深度优先搜索的递归实现

--- 03-图/src/com/mj/graph/ListGraph.java
+++ 03-图/src/com/mj/graph/ListGraph.java
@@ -198,10 +198,21 @@ public class ListGraph<V, E> implements Graph<V, E> {
 
     @Override
     public void dfs(V begin) {
+        // 判断顶点是否存在
+        Vertex<V, E> beginVertex = vertices.get(begin);
+        if (beginVertex == null) return;
 
+        Set<Vertex<V, E>> visitedVertices = new HashSet<>();
+        dfs(beginVertex, visitedVertices);
     }
 
-    private void dfs(){
-        
+    private void dfs(Vertex<V, E> vertex, Set<Vertex<V, E>> visitedVertices) {
+        System.out.println(vertex.value);
+        visitedVertices.add(vertex);
+        for (Edge<V, E> edge : vertex.outEdges) {
+            if (visitedVertices.contains(vertex)) continue;
+            dfs(edge.to, visitedVertices);
+        }
+
     }
 }
