128b4d6d777d101d7359969ee112446c938b5e4c
gao peiwen <vivian9795@163.com>
2020-10-19 16:54:57 +0800

    图的测试遍历

--- 03-图/src/com/mj/Main.java
+++ 03-图/src/com/mj/Main.java
@@ -6,7 +6,12 @@ import com.mj.graph.ListGraph;
 public class Main {
 
     public static void main(String[] args) {
-        test();
+        testBfs();
+    }
+
+    static void testBfs() {
+        Graph<Object, Double> graph = directedGraph(Data.BFS_02);
+        graph.bfs(5);
     }
 
     /**
@@ -45,39 +50,5 @@ public class Main {
             }
         }
         return graph;
-    }
-
-    static void test(){
-        ListGraph<String, Integer> graph = new ListGraph<>();
-        // 无向图
-//        graph.addEdge("V0", "V1");
-//        graph.addEdge("V1", "V0");
-//
-//        graph.addEdge("V0", "V2");
-//        graph.addEdge("V2", "V0");
-//
-//        graph.addEdge("V0", "V3");
-//        graph.addEdge("V3", "V0");
-//
-//        graph.addEdge("V2", "V1");
-//        graph.addEdge("V1", "V2");
-//
-//        graph.addEdge("V2", "V3");
-//        graph.addEdge("V3", "V2");
-//
-//        graph.print();
-
-        // 有向图
-        graph.addEdge("V1", "V0", 9);
-        graph.addEdge("V1", "V2", 3);
-        graph.addEdge("V2", "V0", 2);
-        graph.addEdge("V2", "V3", 5);
-        graph.addEdge("V3", "V4", 1);
-        graph.addEdge("V0", "V4", 6);
-        graph.bfs("V1");
-
-//        graph.removeEdge("V0", "V4");
-//        graph.removeVertex("V0");
-//        graph.print();
     }
 }
