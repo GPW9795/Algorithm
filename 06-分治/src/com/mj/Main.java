2e933a7febffc088780f7c1cfeb88131b5f64ff8
gaopeiwen <vivian9795@163.com>
2020-10-23 22:46:21 +0800

    分治

--- 06-分治/src/com/mj/Main.java
+++ 06-分治/src/com/mj/Main.java
@@ -8,9 +8,48 @@ public class Main {
     }
 
     /**
-     * 暴力出奇迹 - 优化
+     * 分治
      */
     static int maxSubArray(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        return maxSubArray(nums, 0, nums.length);
+    }
+
+    /**
+     * 求解[begin, end)最大子序列和
+     */
+    static int maxSubArray(int[] nums, int begin, int end) {
+        if (end - begin < 2) return nums[begin];
+        int mid = (begin + end) >> 1;
+        // 从mid->begin左边最大值
+        int leftMax = 0;
+        int leftSum = 0;
+        for (int i = mid - 1; i >= begin; i--) {
+            leftSum += nums[i];
+            leftMax = Math.max(leftMax, leftSum);
+        }
+        // 从mid->end 右边最大值
+        int rightMax = 0;
+        int rightSum = 0;
+        for (int i = mid; i < end; i++) {
+            rightSum += nums[i];
+            rightMax = Math.max(rightMax, rightSum);
+        }
+        int max = leftMax + rightMax;
+
+        return Math.max(max, // 中间最大值
+                Math.max(
+                        maxSubArray(nums, begin, mid), // 左边最大值
+                        maxSubArray(nums, mid, end)) // 右边最大值
+        );
+    }
+
+    /**
+     * 暴力出奇迹 - 优化
+     */
+    static int maxSubArray2(int[] nums) {
         if (nums == null || nums.length == 0) {
             return 0;
         }
