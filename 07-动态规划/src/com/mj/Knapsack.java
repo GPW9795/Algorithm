7d177c22618a7a2ed5b0501355d7cd5523d4d849
gao peiwen <vivian9795@163.com>
2020-10-26 17:57:23 +0800

    Update Knapsack.java

--- 07-动态规划/src/com/mj/Knapsack.java
+++ 07-动态规划/src/com/mj/Knapsack.java
@@ -20,8 +20,7 @@ public class Knapsack {
         if (capacity <= 0) return 0;
         int[] dp = new int[capacity + 1];
         for (int i = 1; i <= values.length; i++) {
-            for (int j = capacity; j > 0; j--) {
-                if (j < weights[i - 1]) continue;
+            for (int j = capacity; j >= weights[i - 1]; j--) {
                 dp[j] = Math.max(dp[j], dp[j - weights[i - 1]] + values[i - 1]);
             }
         }
@@ -34,6 +33,8 @@ public class Knapsack {
     static int maxValue1(int[] values, int[] weights, int capacity) {
         if (values == null || values.length == 0) return 0;
         if (weights == null || weights.length == 0) return 0;
+        if (values.length != weights.length) return 0;
+        if (capacity <= 0) return 0;
         int[][] dp = new int[values.length + 1][capacity + 1];
         for (int i = 1; i <= values.length; i++) {
             for (int j = 1; j <= capacity; j++) {
