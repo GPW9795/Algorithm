4bfa58f8b391f3897effd8587acbca377a6abc5a
gao peiwen <vivian9795@163.com>
2020-10-26 16:40:06 +0800

    Create Knapsack.java

---
+++ 07-动态规划/src/com/mj/Knapsack.java
@@ -0,0 +1,14 @@
+package com.mj;
+
+public class Knapsack {
+    public static void main(String[] args) {
+        int[] values = {6, 3, 5, 4, 6};
+        int[] weights = {2, 2, 6, 5, 4};
+        int capacity = 10;
+        System.out.println(maxValue(values, weights, capacity));
+    }
+
+    static int maxValue(int[] values, int[] weights, int capacity) {
+
+    }
+}
