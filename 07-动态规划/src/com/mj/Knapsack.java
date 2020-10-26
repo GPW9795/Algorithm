95a67d77259b6050d1d953a51c02f388973fdf77
gao peiwen <vivian9795@163.com>
2020-10-26 17:30:33 +0800

    01背包

--- 07-动态规划/src/com/mj/Knapsack.java
+++ 07-动态规划/src/com/mj/Knapsack.java
@@ -8,7 +8,22 @@ public class Knapsack {
         System.out.println(maxValue(values, weights, capacity));
     }
 
+    /**
+     * 二维数组
+     */
     static int maxValue(int[] values, int[] weights, int capacity) {
-
+        if (values == null || values.length == 0) return 0;
+        if (weights == null || weights.length == 0) return 0;
+        int[][] dp = new int[values.length + 1][capacity + 1];
+        for (int i = 1; i <= values.length; i++) {
+            for (int j = 1; j <= capacity; j++) {
+                if (j < weights[i - 1]) {
+                    dp[i][j] = dp[i - 1][j];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - weights[i - 1]] + values[i - 1]);
+                }
+            }
+        }
+        return dp[values.length][capacity];
     }
 }
