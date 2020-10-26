f223a88eccd6b9fc1abc4d2834a514ac8d073ff4
gao peiwen <vivian9795@163.com>
2020-10-26 14:53:00 +0800

    dp

--- 07-动态规划/src/com/mj/LCS.java
+++ 07-动态规划/src/com/mj/LCS.java
@@ -8,24 +8,25 @@ public class LCS {
     }
 
     /**
-     * 非递归实现 - 滚动数组优化
+     * 非递归实现 - 一维数组优化
      */
     static int lcs(int[] nums1, int[] nums2) {
         if (nums1 == null || nums1.length == 0) return 0;
         if (nums2 == null || nums2.length == 0) return 0;
-        int[][] dp = new int[2][nums2.length + 1];
+        int[] dp = new int[nums2.length + 1];
         for (int i = 1; i <= nums1.length; i++) {
-            int row = i & 1;
-            int prevRow = (i - 1) & 1;
+            int cur = 0;
             for (int j = 1; j <= nums2.length; j++) {
+                int leftTop = cur; // 左上角的值
+                cur = dp[j];
                 if (nums1[i - 1] == nums2[j - 1]) {
-                    dp[row][j] = dp[prevRow][j - 1] + 1;
+                    dp[j] = leftTop + 1;
                 } else {
-                    dp[row][j] = Math.max(dp[prevRow][j], dp[row][j - 1]);
+                    dp[j] = Math.max(dp[j], dp[j - 1]);
                 }
             }
         }
-        return dp[nums1.length & 1][nums2.length];
+        return dp[nums2.length];
     }
 
     /**
