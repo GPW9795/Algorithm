79eb21817f504fa0c236b5ff59bb7b21d32f75b5
gao peiwen <vivian9795@163.com>
2020-10-26 17:49:52 +0800

    01背包

--- 07-动态规划/src/com/mj/Knapsack.java
+++ 07-动态规划/src/com/mj/Knapsack.java
@@ -1,5 +1,7 @@
 package com.mj;
 
+import javax.print.DocFlavor;
+
 public class Knapsack {
     public static void main(String[] args) {
         int[] values = {6, 3, 5, 4, 6};
@@ -9,9 +11,27 @@ public class Knapsack {
     }
 
     /**
-     * 二维数组
+     * 一维数组
      */
     static int maxValue(int[] values, int[] weights, int capacity) {
+        if (values == null || values.length == 0) return 0;
+        if (weights == null || weights.length == 0) return 0;
+        if (values.length != weights.length) return 0;
+        if (capacity <= 0) return 0;
+        int[] dp = new int[capacity + 1];
+        for (int i = 1; i <= values.length; i++) {
+            for (int j = capacity; j > 0; j--) {
+                if (j < weights[i - 1]) continue;
+                dp[j] = Math.max(dp[j], dp[j - weights[i - 1]] + values[i - 1]);
+            }
+        }
+        return dp[capacity];
+    }
+
+    /**
+     * 二维数组
+     */
+    static int maxValue1(int[] values, int[] weights, int capacity) {
         if (values == null || values.length == 0) return 0;
         if (weights == null || weights.length == 0) return 0;
         int[][] dp = new int[values.length + 1][capacity + 1];
