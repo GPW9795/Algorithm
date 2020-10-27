3b88df3f746776abcff1808a7421ff6f3b6aeb51
gao peiwen <vivian9795@163.com>
2020-10-27 11:40:12 +0800

    sl

---
+++ 09-跳表/src/com/mj/SkipList.java
@@ -0,0 +1,78 @@
+package com.mj;
+
+import java.util.Comparator;
+
+public class SkipList<K, V> {
+    private static final int MAX_LEVEL = 32;
+    private int size;
+    private Comparator<K> comparator;
+    /**
+     * 有效层数
+     */
+    private int level;
+    /**
+     * 不存放任何key
+     */
+    private Node<K, V> first;
+
+    public SkipList() {
+        this(null);
+    }
+
+    public SkipList(Comparator<K> comparator) {
+        this.comparator = comparator;
+        first = new Node<>();
+        first.nexts = new Node[MAX_LEVEL];
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public V put(K key, V value) {
+        keyCheck(key);
+        return null;
+    }
+
+    public V get(K key) {
+        keyCheck(key);
+
+        Node<K, V> node = first;
+        for (int i = level - 1; i >= 0; i--) {
+            int cmp = -1;
+            while (node.nexts[i] != null && (cmp = compare(key, node.nexts[i].key)) > 0) {
+                node = node.nexts[i];
+            }
+            // node.nexts[i] == null || node.nexts[i].key >= key
+            if (cmp == 0) {
+                return node.nexts[i].value;
+            }
+        }
+        return null;
+    }
+
+    public V remove(K key) {
+        keyCheck(key);
+        return null;
+    }
+
+    private void keyCheck(K key) {
+        if (key == null) {
+            throw new IllegalArgumentException("Key must not be null.");
+        }
+    }
+
+    private int compare(K k1, K k2) {
+        return comparator != null ? comparator.compare(k1, k2) : ((Comparable<K>) k1).compareTo(k2);
+    }
+
+    private static class Node<K, V> {
+        K key;
+        V value;
+        Node<K, V>[] nexts;
+    }
+}
