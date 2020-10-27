1d1e1f65341793e86c9896e66360855f95faca7e
gao peiwen <vivian9795@163.com>
2020-10-27 17:19:05 +0800

    蛮力1优化

--- 10-字符串/src/com/mj/BruteForce01.java
+++ 10-字符串/src/com/mj/BruteForce01.java
@@ -1,7 +1,10 @@
 package com.mj;
 
 public class BruteForce01 {
-    public static int indexOf(String text, String pattern) {
+    /**
+     * 最初
+     */
+    public static int indexOf1(String text, String pattern) {
         if (text == null || pattern == null) return -1;
         char[] textChars = text.toCharArray();
         int tlen = textChars.length;
@@ -13,6 +16,34 @@ public class BruteForce01 {
 
         int pi = 0, ti = 0;
         while (pi < plen && ti < tlen) {
+            if (textChars[ti] == patternChars[pi]) {
+                ti++;
+                pi++;
+            } else {
+                ti -= pi - 1;
+                pi = 0;
+            }
+        }
+        return pi == plen ? ti - pi : -1;
+    }
+
+
+    /**
+     * 优化
+     */
+    public static int indexOf(String text, String pattern) {
+        if (text == null || pattern == null) return -1;
+        char[] textChars = text.toCharArray();
+        int tlen = textChars.length;
+        if (tlen == 0) return -1;
+        char[] patternChars = pattern.toCharArray();
+        int plen = patternChars.length;
+        if (plen == 0) return -1;
+        if (tlen < plen) return -1;
+
+        int pi = 0, ti = 0;
+        int tmax = tlen - plen;
+        while (pi < plen && ti - pi <= tmax) {
             if (textChars[ti] == patternChars[pi]) {
                 ti++;
                 pi++;
